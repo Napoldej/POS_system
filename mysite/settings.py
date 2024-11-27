fa1184fcfde82d73701d90ce3651cfed9cab06c4
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-11-27 22:48:27 +0700

    ADd module

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-gq)#cl+i%m+$r#iu)9_@hebicd)zch%69^rvcz6p(=*@!vksx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DATABASE_NAME', 'django_db'),
+            'USER': os.getenv('DATABASE_USER', 'django_user'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'django_password'),
+            'HOST': os.getenv('DATABASE_HOST', 'db'),
+            'PORT': os.getenv('DB_PORT', '5432'),  
     }
 }
 
